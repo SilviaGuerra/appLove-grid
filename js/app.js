e921a67ed93e2180a508e1c7d1f14f673fdc9aee
SilviaGuerra <silvia.guerrahdz@gmail.com>
2017-05-04 00:39:36 -0500

    agregue modal

--- js/app.js
+++ js/app.js
@@ -1,3 +1,19 @@
 /*
  * Archivo principal de funcionalidad de JS
  */
+var imgs = document.getElementsByClassName("image");
+
+for(i = 0; i < imgs.length; i++){
+   imgs[i].addEventListener("click", abrir);
+}
+
+function abrir(){
+  document.getElementById("myModal").style.display="block";
+  document.getElementById("imageZoom").src = this.src
+}
+
+var span = document.getElementsByClassName("close")[0];
+
+span.onclick = function() {
+document.getElementById("myModal").style.display = "none";
+}
